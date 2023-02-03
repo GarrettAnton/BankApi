f2d7a49d72ec828cb4736ef8488e48861d421b99
Admin <Admin@gmail.com>
2023-02-03 17:40:10 +0200

    Add PrivateBankReciever

---
+++ RestLib/Recievers/PrivateBankUkraine/Models/PrivateBankCurrency.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace RestLib.Recievers.PrivateBankUkraine.Models
+{
+    public class PrivateBankCurrency
+    {
+        [JsonProperty("ccy")]
+        public string Ccy { get; set; }
+
+        [JsonProperty("base_ccy")]
+        public string BaseCcy { get; set; }
+
+        [JsonProperty("buy")]
+        public string Buy { get; set; }
+
+        [JsonProperty("sale")]
+        public string Sale { get; set; }
+    }
+}
